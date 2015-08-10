610905f47fe7498ea72dcd9a6770785c6386b6ed
Antonio Eggermont <aarandaeggermont@gmail.com>
2015-08-10 09:24:06 -0700

    removing test database pets

--- app/src/main/java/com/eggermont/virtinsight/AlbumTrackerDatabase.java
+++ app/src/main/java/com/eggermont/virtinsight/AlbumTrackerDatabase.java
@@ -13,24 +13,6 @@ public final class AlbumTrackerDatabase {
 	private AlbumTrackerDatabase() {}
 
 
-	// Pets table
-	public static final class Pets implements BaseColumns {
-		private Pets() {}
-		public static final String PETS_TABLE_NAME = "table_pets";
-		public static final String PET_NAME = "pet_name";
-		public static final String PET_TYPE_ID = "pet_type_id";
-		public static final String DEFAULT_SORT_ORDER = "pet_name ASC";
-	}
-
-	// Pet Type table
-	public static final class PetType implements BaseColumns {
-		private PetType() {}
-		public static final String PETTYPE_TABLE_NAME = "table_pettypes";
-		public static final String PET_TYPE_NAME = "pet_type";
-		public static final String DEFAULT_SORT_ORDER = "pet_type ASC";
-	}
-
-
 	// Albums table definitions
 	public static final class VirtAlbums implements BaseColumns{
 		private VirtAlbums() {}
