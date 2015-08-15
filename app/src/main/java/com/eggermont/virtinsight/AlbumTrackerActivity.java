f8920305e1678b3f9c486bc8307fcabda8150b44
Antonio Eggermont <aarandaeggermont@gmail.com>
2015-08-15 15:20:42 -0700

    added method to search events by album ID

--- app/src/main/java/com/eggermont/virtinsight/AlbumTrackerActivity.java
+++ app/src/main/java/com/eggermont/virtinsight/AlbumTrackerActivity.java
@@ -10,9 +10,12 @@ import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TableLayout;
 
+import java.text.ParseException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
+import java.text.SimpleDateFormat;
 
 /**
  *  Manages the instantiation and closing of a local
@@ -35,11 +38,13 @@ public class AlbumTrackerActivity extends Activity {
 	}
 
 	/**
-	 * This method adds a new event to an existing album
+	 * This method inserts a new event to an existing album into the
+	 * album events table
 	 *
 	 * @param albumId
 	 * @param currentPhotoPath
 	 * @param txtSpeechInput
+	 *
 	 * @return
 	 */
 	public long addNewEvent(long albumId, String currentPhotoPath, String txtSpeechInput ){
@@ -78,7 +83,84 @@ public class AlbumTrackerActivity extends Activity {
 
 
 	/**
-	 * This method gets a list of all albums availbale in the database
+	 * This method fetches all media events stored in  the database for a
+	 * specific album
+	 *
+	 * @param albumId unique identifier for a stored album
+	 */
+	public TreeMap<Integer, HashMap<String, String>> getEventsForAlbum(long albumId){
+
+		TreeMap albumEvents = new TreeMap<Integer, HashMap<String, String>>();
+		SimpleDateFormat dateformat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
+		long epoch = 0;
+
+		Log.i(DEBUG_TAG, "About to fetch events for album ID: " + albumId );
+
+		// SQL Query to fetch album events from the database
+		SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+		queryBuilder.setTables(AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_TABLE_NAME);
+
+		// Get the Database and run the query
+		SQLiteDatabase db = mDatabase.getReadableDatabase();
+
+		String asColumnsToReturn[] = {
+				AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_TABLE_NAME + "." + AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_ALBUM_ID,
+				AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_TABLE_NAME + "." + AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_AUDIO_SPEECH,
+				AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_TABLE_NAME + "." + AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_PHOTO_LINK,
+				AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_TABLE_NAME + "." + AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_DATE_ADDED,
+				AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_TABLE_NAME + "." + AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_EVENT_GPS,
+				AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_TABLE_NAME + "." + AlbumTrackerDatabase.AlbumEvents._ID
+		};
+
+		queryBuilder.appendWhere(
+				AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_ALBUM_ID + "=" +  albumId);
+
+		Cursor eventRows = queryBuilder.query(db, asColumnsToReturn, null, null, null, null, null, null);
+
+
+		if (eventRows.moveToFirst()) {
+			do {
+				Date date = null;
+				HashMap<String, String> eventRecord = new HashMap<String, String>();
+
+				try {
+					date = dateformat.parse(eventRows.getString(eventRows.getColumnIndex(AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_DATE_ADDED)));
+					epoch = date.getTime()/100;
+
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+
+				Log.i(DEBUG_TAG, "Album ID " + eventRows.getInt(eventRows.getColumnIndex(AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_ALBUM_ID)));
+				Log.i(DEBUG_TAG, "Evemt ID " + eventRows.getInt(eventRows.getColumnIndex(AlbumTrackerDatabase.AlbumEvents._ID)));
+				Log.i(DEBUG_TAG, eventRows.getString(eventRows.getColumnIndex(AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_AUDIO_SPEECH)));
+				Log.i(DEBUG_TAG, eventRows.getString(eventRows.getColumnIndex(AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_PHOTO_LINK)));
+				Log.i(DEBUG_TAG, eventRows.getString(eventRows.getColumnIndex(AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_DATE_ADDED)));
+				Log.i(DEBUG_TAG, "Epoch Time: " + epoch);
+				Log.i(DEBUG_TAG, eventRows.getString(eventRows.getColumnIndex(AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_EVENT_GPS)));
+
+				eventRecord.put("album_id", eventRows.getString(eventRows.getColumnIndex(AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_ALBUM_ID)));
+				eventRecord.put("date_added",eventRows.getString(eventRows.getColumnIndex(AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_DATE_ADDED)));
+				eventRecord.put("photo_path", eventRows.getString(eventRows.getColumnIndex(AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_PHOTO_LINK)));
+				eventRecord.put("gps_location", eventRows.getString(eventRows.getColumnIndex(AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_EVENT_GPS)));
+				eventRecord.put("description", eventRows.getString(eventRows.getColumnIndex(AlbumTrackerDatabase.AlbumEvents.ALBUMEVENTS_AUDIO_SPEECH)));
+				albumEvents.put(epoch, eventRecord);
+			}while (eventRows.moveToNext());
+		}
+
+		Log.i(DEBUG_TAG, "Count of album evemt records: " + eventRows.getCount());
+		Log.i(DEBUG_TAG, "asColumnsToReturn : " + asColumnsToReturn.toString());
+
+		eventRows.close();
+		db.close();
+
+		return albumEvents;
+
+	}
+
+
+	/**
+	 * This method gets a list of all albums available in the database
 	 *
 	 * @return a hashmap with all album records by album ids as keys
 	 */
@@ -92,12 +174,13 @@ public class AlbumTrackerActivity extends Activity {
 		// Get the Database and run the query
 		SQLiteDatabase db = mDatabase.getReadableDatabase();
 
-		String asColumnsToReturn[] = {AlbumTrackerDatabase.VirtAlbums.ALBUMS_TABLE_NAME + "." + AlbumTrackerDatabase.VirtAlbums.ALBUM_TITLE_NAME,
+		String asColumnsToReturn[] = {
+				AlbumTrackerDatabase.VirtAlbums.ALBUMS_TABLE_NAME + "." + AlbumTrackerDatabase.VirtAlbums.ALBUM_TITLE_NAME,
 				AlbumTrackerDatabase.VirtAlbums.ALBUMS_TABLE_NAME + "." + AlbumTrackerDatabase.VirtAlbums.ALBUM_DESCRIPTION,
 				AlbumTrackerDatabase.VirtAlbums.ALBUMS_TABLE_NAME + "." + AlbumTrackerDatabase.VirtAlbums.ALBUM_DATE_ADDED,
-				AlbumTrackerDatabase.VirtAlbums.ALBUMS_TABLE_NAME + "." + AlbumTrackerDatabase.VirtAlbums._ID};
+				AlbumTrackerDatabase.VirtAlbums.ALBUMS_TABLE_NAME + "." + AlbumTrackerDatabase.VirtAlbums._ID
+		};
 
-		//Cursor c = queryBuilder.query(db, asColumnsToReturn, null, null, null, null, VirtAlbums.DEFAULT_SORT_ORDER);
 
 		Cursor allRows = queryBuilder.query(db, asColumnsToReturn, null, null, null, null, null, null);
 
@@ -108,6 +191,7 @@ public class AlbumTrackerActivity extends Activity {
 			// Iterate over each cursor
 			do {
 				HashMap<String, String> record = new HashMap<String, String>();
+
 				record.put("album_id", Integer.toString(allRows.getInt(allRows.getColumnIndex(AlbumTrackerDatabase.VirtAlbums._ID))));
 				record.put("title_name", allRows.getString(allRows.getColumnIndex(AlbumTrackerDatabase.VirtAlbums.ALBUM_TITLE_NAME)));
 				record.put("date_added", allRows.getString(allRows.getColumnIndex(AlbumTrackerDatabase.VirtAlbums.ALBUM_DATE_ADDED)));
@@ -169,7 +253,10 @@ public class AlbumTrackerActivity extends Activity {
 	}
 
 
-
+	/**
+	 * TODO: This method is duplicated and should be removed
+	 * @return
+	 */
 	public String [] getStoredAlbums(){
 
 		// SQL Query to fetch albums from database
