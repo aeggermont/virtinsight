f035a21f749ef2e2db0b118d6c202df8629d60c8
Antonio Eggermont <aarandaeggermont@gmail.com>
2015-08-17 17:30:46 -0700

    adding image viewer display target proprtions

--- app/src/main/java/com/eggermont/virtinsight/AlbumViewer.java
+++ app/src/main/java/com/eggermont/virtinsight/AlbumViewer.java
@@ -2,6 +2,7 @@ package com.eggermont.virtinsight;
 
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,8 +15,9 @@ import java.util.TreeMap;
 import android.widget.AdapterView;
 import android.view.View;
 import android.app.Activity;
+import android.widget.TextView;
 import android.widget.Toast;
-
+import android.widget.ImageView;
 
 /**
  *  This class is used to view stored albums and their contents. This
@@ -36,6 +38,11 @@ public class AlbumViewer extends AlbumTrackerActivity {
     private long albumId;
     private String albumName;
     private String albumDesc;
+    private int imageWidth;
+    private static final float INITIAL_ITEMS_COUNT = 4.5F;
+    private TreeMap<Integer, HashMap<String, String>> albumEvents;
+    private TextView mDescription;
+    private GridView mGridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,35 +50,40 @@ public class AlbumViewer extends AlbumTrackerActivity {
         setContentView(R.layout.activity_album_viewer);
 
         Intent albumInt = getIntent();
-        TreeMap<Integer, HashMap<String, String>> albumEvents;
-
         this.albumName = albumInt.getExtras().getString("albumName");
         this.albumId = albumInt.getExtras().getLong("albumId");
         this.albumDesc = albumInt.getExtras().getString("albumDesc");
 
+        // Compute the width of a carousel item based on the screen width and number of initial items.
+        final DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        this.imageWidth = (int) (displayMetrics.widthPixels / INITIAL_ITEMS_COUNT);
+
+
+        // TODO: Retrieving media from the activity should be done in the ImageAdapter
+        albumEvents = getEventsForAlbum(albumId);
+
         Log.i(DEBUG_TAG, "Just got intent: " + albumId + " : " + albumName);
 
-        // Get events from database
-        albumEvents =  getEventsForAlbum(albumId);
-        Log.i(DEBUG_TAG, "Events: " +  albumEvents.toString());
+        mGridView = (GridView) findViewById(R.id.gridViewPhotos);
+        this.mDescription = (TextView) findViewById(R.id.eventDesc);
 
-
-        GridView mGridView = (GridView) findViewById(R.id.gridViewPhotos);
-        mGridView.setAdapter(new ImageAdapter(this));
+        mGridView.setAdapter(new ImageAdapter(this, albumEvents, this.imageWidth, this.albumName ));
 
         mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                view.getTag();
                 Toast.makeText(AlbumViewer.this, "" + position,
                         Toast.LENGTH_SHORT).show();
 
+                EventTag eventTag = (EventTag)view.getTag();
+                mDescription.setText(eventTag.getDescription());
+                Log.i(DEBUG_TAG, "Clicking " + eventTag.getDescription());
+                Log.i(DEBUG_TAG, "Clicking " + eventTag.getDateAdded());
+                Log.i(DEBUG_TAG, "Clicking " + eventTag.getGelocation_info());
             }
         });
-
-
-        //Log.i(DEBUG_TAG, "First Event: " + albumEvents.firstKey().toString());
-        //Log.i(DEBUG_TAG, "Last Event: " +  albumEvents.lastEntry().toString());
-
     }
 
 
@@ -92,19 +104,32 @@ public class AlbumViewer extends AlbumTrackerActivity {
                 events.putExtra("albumId", this.albumId);
                 events.putExtra("albumName", this.albumName);
                 events.putExtra("albumDesc", this.albumDesc);
+                startActivity(events);
                 super.onDestroy();
                 return true;
 
             case  R.id.home:
                 Log.i(DEBUG_TAG, "Returning to main menu");
                 Intent home = new Intent(AlbumViewer.this, AlbumInventoryActivity.class);
-                super.onDestroy();
                 startActivity(home);
 
             case R.id.view_album:
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        GridView gridView = (GridView) findViewById(R.id.gridViewPhotos);
+        Log.i(DEBUG_TAG, "Calling on Destroy!");
+
+        int count = gridView.getCount();
+        for (int i = 0; i < count; i++){
+
+            final ImageView v = (ImageView) gridView.getChildAt(i);
+            v.getDrawable().setCallback(null);
         }
     }
 }
